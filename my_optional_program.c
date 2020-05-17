122fc8b924267c3493118edb17e107b57a4eaba4
Natasha Ong <56423291+natashasaki@users.noreply.github.com>
2020-05-17 19:57:08 +0900

    Update my_optional_program.c

--- my_optional_program.c
+++ my_optional_program.c
@@ -1,17 +1,7 @@
 /* File: my_optional_program.c
  * ---------------------------
- * This program is compiled to use your custom heap allocator
- * when allocating memory.  Optionally write any program you'd
- * like below that allocates memory, using mymalloc, myrealloc,
- * and myfree instead of malloc, realloc and free, and see your
- * heap allocator in action!  (Note: if your code uses indirect 
- * heap-allocating functions like calloc or strdup, you will have
- * to implement this functionality in other ways to have it use
- * your custom heap allocator). 
- *
- * When you compile using `make`, it will create 3 different
- * compiled versions of this program, one using each type of
- * heap allocator.
+ * This program is compiled to use custom heap allocator
+ * when allocating memory.  
  */
 
 #include <stdio.h>
@@ -29,10 +19,7 @@ bool initialize_heap_allocator() {
 int main(int argc, char *argv[]) {
     if (!initialize_heap_allocator()) {
         return 1;
-    }
-
-    // optionally write your code here!
-    
+    }    
     return 0;
 }
 
